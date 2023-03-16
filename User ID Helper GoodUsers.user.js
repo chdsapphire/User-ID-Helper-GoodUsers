aa4ef3b4ebcf6307c65cf344c724894edcbc3891
chdsapphire <123945062+chdsapphire@users.noreply.github.com>
2023-03-17 00:23:38 +0200

    Update User ID Helper GoodUsers.user.js

--- User ID Helper GoodUsers.user.js
+++ User ID Helper GoodUsers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name User ID Helper GoodUsers
 // @namespace http://example.com/
-// @version 2.23
+// @version 2.24
 // @description Adds a helper message near user ID
 // @author Me
 // @match http://www.charmdate.com/**
